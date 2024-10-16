145239cde54647020f58bd5b36647590b8a1ac22
Artem468 <56316884+Artem468@users.noreply.github.com>
2024-10-16 19:27:02 +0200

    Update Program.cs

--- TestingLab2/Program.cs
+++ TestingLab2/Program.cs
@@ -2,15 +2,9 @@
 
 int[,] data = Convertor.FromArrayToMatrix(new[] { 1, 2, 3, 4, 5, 6, 7, 8, 9 }, 4)!;
 
-foreach (var item in data)
+for (int i = 0; i < 4; i++)
 {
-    Console.Write($"{item} ");
-}
-Console.WriteLine();
-
-for (int i = 0; i < 3; i++)
-{
-    for (int j = 0; j < 3; j++)
+    for (int j = 0; j < 4; j++)
     {
         Console.Write(data[i, j] + " ");
     }
